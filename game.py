1d893122548622a04f2cd3589b5044bc2f5a6a73
Dipesh <dipeshreen@gmail.com>
2016-05-12 09:45:09 +0530

    Get user inputs

--- game.py
+++ game.py
@@ -1,6 +1,26 @@
-def generate_option_sequence():
-	# THe order is important as every 'x' defeats only 'x+1' and 'x+2'
-	return 'Rock', 'Scissor', 'Lizzard', 'Paper', 'Spock'
+import random
+
+class Game(object):
+	"""docstring for Game"""
+
+	def __init__(self, game_no):
+		self.game_no = game_no
+		print ("Game", self.game_no)
+
+	def __option_sequence(self):
+		""" Return a tuple of possible options """
+		#  The order is important as every 'x' defeats only 'x+1' and 'x+2'
+		return 'Rock', 'Scissor', 'Lizzard', 'Paper', 'Spock'
+
+	def get_choices(self):
+		""" get users choice and randomly generated computr choice """
+		self.user_option = input("Enter your choice : ")
+		if self.user_option == 0:
+			# exit()
+		self.comp_option = random.randint(0,4)
+
+	def compare_choices(self):
+		pass
 
 def rules():
 	# Rules straight out of 'Big Bang Theory'
@@ -23,6 +43,14 @@ def rules():
 
 def main():
 	print (rules())
+	no_of_games = int(input("How many games do you want to play? : "))
+	if no_of_games != 0:
+		print ("Enter 0 to quit")
+	for i in range(no_of_games):
+		game = Game(i)
+		game.get_choices()
+		game.compare_choices()
+
 	
 
 if __name__ == '__main__':
